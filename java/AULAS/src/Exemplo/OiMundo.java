9addbb24ca982d161bc19651ede2a653159e6f0c
coder samuel <samuelsantos.ss95@gmail.com>
2021-03-24 16:46:08 -0300

    primeiro commit java

---
+++ java/AULAS/src/Exemplo/OiMundo.java
@@ -0,0 +1,12 @@
+package Exemplo;
+
+public class OiMundo {
+	
+	public static void main(String[] args) {
+		
+		//escreva 	
+		System.out.println("Oi, turma 19 generation!");
+		System.out.println("Vamos programar em Java!");
+	}
+	
+}
